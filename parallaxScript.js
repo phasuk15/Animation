c3bf2c7f753ea80cada420062405cf16466ee65d
phasuk15 <phasukdianas15@gmail.com>
2024-07-01 12:06:02 +0100

    added more layers and speed modifiers

--- parallaxScript.js
+++ parallaxScript.js
@@ -1,9 +1,13 @@
+
+//parallax effect is when the foreground moves faster than the background creating a 3d effect 
+
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 15;
+let gameSpeed = 5;
+let gameFrame = 0;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'layer-1.png';
@@ -16,27 +20,70 @@ backgroundLayer4.src = 'layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'layer-5.png';
 
-//uses two images for endless scrolling animation
-let x = 0;
-let x2 = 2400;
+//
+window.addEventListener('load', function(e){
 
-function animate() {
-    //clears previous frame
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-    ctx.drawImage(backgroundLayer4, x, 0);  
-    ctx.drawImage(backgroundLayer4, x2, 0);  
+    //shared properties between the layers
+    class Layer {
+        constructor(image, speedModifier) {
+            this.x = 0;
+            this.y = 0;
+            this.width = 2400;
+            this.height = 700;
+            this.x2 = this.width;
+            this.image = image;
+            this.speedModifier = speedModifier;
+            this.speed = gameSpeed * this.speedModifier;
+        }
+    
+        //method wll change the layers horizontally and endlessy scroll
+        update() {
+            this.speed = gameSpeed * this.speedModifier;
+            // if (this.x <= -this.width) {
+            //     this.x = 0;
+            // }
+            this.x = gameFrame * this.speed % this.width;
+        }
+    
+        //draw method will take image and draw on the canvas
+        draw() {
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+        }
+        
+    }
+    
+    const layer1 = new Layer(backgroundLayer1, 0.2);
+    const layer2 = new Layer(backgroundLayer2, 0.4);
+    const layer3 = new Layer(backgroundLayer3, 0.6);
+    const layer4 = new Layer(backgroundLayer4, 0.8);
+    const layer5 = new Layer(backgroundLayer5, 1);;
+    
+    const gameObjects = [layer1, layer2, layer3, layer4, layer5];
+    
+    const slider = document.getElementById('slider');
+    slider.value = gameSpeed;
+    const showGameSpeed = document.getElementById('showGameSpeed');
+    showGameSpeed.innerHTML = gameSpeed;
+    //innerHTML represents HTML content inside element
+    slider.addEventListener('change', function(e) {
+        gameSpeed = e.target.value;
+        showGameSpeed.innerHTML = gameSpeed;
+    });
+    
+    function animate() {
+        //clears previous frame
+        ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+        gameObjects.forEach(object => {
+            object.update();
+            object.draw();
+        });
+        gameFrame--;
+        //loops the animation
+        requestAnimationFrame(animate);
+    };
+    
+    animate();
+});
 
-    if (x < -2400) 
-        x = 2400 - gameSpeed + x2;
-    else 
-        x -= gameSpeed;
-    if (x2 < -2400) 
-        x2 = 2400 - gameSpeed + x;
-    else 
-        x2 -= gameSpeed;
 
-    //loops the animation
-    requestAnimationFrame(animate);
-};
-
-animate();
