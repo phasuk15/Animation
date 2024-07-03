7f79149cc3b8819ca0f9e2bf6d88efb1c5faa2db
phasuk15 <phasukdianas15@gmail.com>
2024-07-03 01:57:44 +0100

    endless scroll animation with all layers

--- parallaxScript.js
+++ parallaxScript.js
@@ -30,7 +30,6 @@ window.addEventListener('load', function(e){
             this.y = 0;
             this.width = 2400;
             this.height = 700;
-            this.x2 = this.width;
             this.image = image;
             this.speedModifier = speedModifier;
             this.speed = gameSpeed * this.speedModifier;
@@ -42,13 +41,15 @@ window.addEventListener('load', function(e){
             // if (this.x <= -this.width) {
             //     this.x = 0;
             // }
+            // this.x = this.x - this.speed;
+
             this.x = gameFrame * this.speed % this.width;
         }
     
         //draw method will take image and draw on the canvas
         draw() {
             ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-            ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
         }
         
     }
