d256b5f51c748dac65f277f308f105f401b3f30d
phasuk15 <phasukdianas15@gmail.com>
2024-07-04 19:53:53 +0100

    flying creature 4

--- flyingScript.js
+++ flyingScript.js
@@ -6,7 +6,7 @@ const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
 
-const numberOfEnemies = 100;
+const numberOfEnemies = 20;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -14,30 +14,33 @@ let gameFrame = 0;
 class Enemy {
     constructor() {
         this.image = new Image();
-        this.image.src = 'enemy3.png'
+        this.image.src = 'enemy4.png'
         this.speed = Math.random() * 4 + 1;
-        this.spriteWidth = 218;
-        this.spriteHeight = 177;
+        this.spriteWidth = 213;
+        this.spriteHeight = 213;
         //width and height is changed so that image is not stretched
         this.width = this.spriteWidth/2;
         this.height = this.spriteHeight/2;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
+        this.newX = Math.random() * (canvas.width);
+            this.newY = Math.random() * (canvas.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-        this.angle = 0;
-        this.angleSpeed = Math.random() * 1.5 + 0.5;
-        // this.curve = Math.random() * 1.5 + 0.5;
+        this.interval = Math.floor(Math.random() * 200 + 50);
     }
     update() {
-        //side to side movement within a certain range
-        this.x = canvas.width/2 * Math.cos(this.angle * Math.PI/180) + (canvas.width/2 - this.width/2);
-        this.y = canvas.height/2 * Math.sin(this.angle * Math.PI/180) + (canvas.height/2 - this.height/2);
-        this.angle += this.angleSpeed;
+        if (gameFrame % this.interval === 0) {
+            this.newX = Math.random() * (canvas.width - this.width);
+            this.newY = Math.random() * (canvas.height - this.height);
+        }
+        this.dx = this.x - this.newX;
+        this.dy = this.y - this.newY;
+        this.x -= this.dx/20;
+        this.y -= this.dy/20;
+        // this.x = 0;
+        // this.y = 0;
         if (this.x + this.width < 0) this.x = canvas.width;
-        //up and down movement
-        // this.y += this.curve * Math.sin(this.angle);
-        this.angle += this.angleSpeed;
         //moves to next frame only if loop is divisible by flapSpeed
         if (gameFrame % this.flapSpeed === 0) {
             //if frame = 4 then frame = 0, else frame++
